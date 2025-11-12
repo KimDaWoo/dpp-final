a21b61171bea9483fcded2d8a369103d9bef14d0
KimDaWoo <dawoo0000@naver.com>
2025-11-12 09:58:03 +0900

    FIX: calendar UI

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -21,18 +21,18 @@ function Calendar({
       className={cn("p-3", className)}
       classNames={{
         root: "rdp-root",
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        months: "flex flex-col sm:flex-row space-y-4 sm:space-y-0 px-3 py-2",
         month: "space-y-4",
-        month_caption: "flex justify-center pt-1 relative items-center",
+        month_caption: "flex justify-center pt-1 relative items-center z-[1]",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         button_previous: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-6 top-5.5 z-[2]"
         ),
         button_next: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-6 top-5.5 z-[2]"
         ),
         month_grid: "w-full border-collapse space-y-1",
         weekdays: "flex",
